5c465eccac2869c43a2ded74867adb6989bd4322
Mauro Parafati <mparafati@fi.uba.ar>
2024-02-17 22:53:58 +0100

    API.GetSchemaByID and API.DeleteSchemaByID implemented

--- internal/api/schema.go
+++ internal/api/schema.go
@@ -40,3 +40,42 @@ func (s *SchemaServer) CreateSchema(ctx context.Context, req *schema_service.Cre
 	fmt.Println("END CreateSchema API")
 	return response, nil
 }
+
+func (s *SchemaServer) GetSchemaByID(ctx context.Context, req *schema_service.GetSchemaByIDRequest) (*schema_service.GetSchemaByIDResponse, error) {
+	fmt.Println("START GetSchemaByID API")
+
+	// Invoke SchemaHandler for fetching the schema
+	schema, err := s.SchemaHandler.GetByID(int(req.SchemaId))
+	if err != nil {
+		fmt.Println("Error calling SchemaHandler.GetByID: ", err)
+		return nil, err
+	}
+
+	// Create and return gRPC response object
+	response := &schema_service.GetSchemaByIDResponse{
+		SchemaId: req.SchemaId,
+		Schema:   domain.SchemaToGRPC(&schema),
+	}
+
+	fmt.Println("END GetSchemaByID API")
+	return response, nil
+}
+
+func (s *SchemaServer) DeleteSchemaByID(ctx context.Context, req *schema_service.DeleteSchemaByIDRequest) (*schema_service.DeleteSchemaByIDResponse, error) {
+	fmt.Println("START DeleteSchemaByID API")
+
+	// Invoke SchemaHandler for deleting the schema
+	err := s.SchemaHandler.DeleteByID(int(req.SchemaId))
+	if err != nil {
+		fmt.Println("Error calling SchemaHandler.DeleteByID: ", err)
+		return nil, err
+	}
+
+	// Create and return gRPC response object
+	response := &schema_service.DeleteSchemaByIDResponse{
+		SchemaId: req.SchemaId,
+	}
+
+	fmt.Println("END DeleteSchemaByID API")
+	return response, nil
+}
